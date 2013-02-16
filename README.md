267a6a35544488592a17c989c2db623d11849298
Matus Goljer <dota.keys@gmail.com>
2013-02-16 23:23:23 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
-indicators.el
-=============
+# indicators.el
 
-Manage indicators in the fringe (=margin) relative to the buffer size.
+In emacs, displaying fringe indicators is done via text overlays. In that way, bitmaps in the fringe are attached to the lines of text shown in the buffer window.
+
+This works nicely when the fringe is used to indicate information that is relevant to the adjacent line; for example, text overflow, or something similar. But, there isn't a simple way for an application or module to use the fringe to display buffer-relative information -- for example, the location of compiler error messages.
+
+Using the fringe to communicate this kind of information -- buffer-relative positions -- is probably more intuitive and is certainly more useful for the user.
+
+This package is heavily inspired by [RFringe](http://www.emacswiki.org/emacs/RFringe). However, there are some notable differences:
+
+* **indicators.el offers per-pixel resolution. This means it's able to display multiple indicators on one line instead of just one for all that would potentionally fall there (as RFringe does).**
+* indicators.el provides minor mode `indicators-mode` so you can quickly remove and disable the display of managed indicators. You can still manage your indicators manually and call `(ind-update my-indicators)` to update them manually (or from your own package).
+* Instead of only positions, you can add indicators to lines, markers or "functions".
+    * Indicators on lines always point to this line.
+    * Indicators on markers move if the buffer content is updated (using standard Emacs LISP markers).
+    * Indicators on functions compute their position from a function. To add a simple indicator of buffer-relative position you can therefore call `(ind-create-indicator 'point :managed t)`.
+* You can specify the face for the indicator.
+
+# How to update indicators
+
+To turn on automatic updates of managed indicators simply enable `indicators-mode` in the buffer.
+
+To manually update lists of your own indicators or indicators managed by your package call `(ind-update my-list-of-indicators)`. The items on this list should be indicators as returned by some of the `ind-create-indicator[-*]` function (see next section).
+
+# How to create indicators
+
+To create your own indicator use:
+
+```scheme
+;; create static indicator at position 1337
+(ind-create-indicator 1337)
+
+;; create static indicator at line 15
+(ind-create-indicator-at-line 15)
+
+;; create dynamic indicator at marker with initial position 2238
+(ind-create-indicator-at-marker 2238)
+```
+
+All indicators are automatically buffer-local.
+
+If you want `indicators-mode` to automatically manage the indicator, use the keyword argument `:managed t`
+
+```scheme
+;; create managed static indicator at position of (point).  Each time
+;; `ind-update' is called this value is recomputed using `point'
+;; function
+(ind-create-indicator 'point :managed t)
+```
+
+To style your indicators use keyword argument `:face name-of-face`. If multiple indicators fall on the same physical line they will inherit the color of the indicator with highest priority (this is a limitation of emacs fringes and cannot be fixed). You can specify the priority with keyword argument `:priority number`. Default priority is 10, default face is `font-lock-warning-face`.
+
+```scheme
+(ind-create-indicator-at-line 12 :face font-lock-constant-face :priority 100)
+```
---
+++ indicators.el
@@ -0,0 +1,250 @@
+;;; indicators.el --- display the relative location of line in the fringe.
+
+;; Copyright (C) 2013 Matus Goljer
+
+;; Author: Matus Goljer <matus.goljer@gmail.com>
+;; Maintainer: Matus Goljer <matus.goljer@gmail.com>
+;; Created: 16 Feb 2013
+;; Version: 0.0.1
+;; Keywords: fringe frames
+;; URL: https://github.com/Fuco1/indicators.el
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See github readme at https://github.com/Fuco1/indicators.el
+
+;; Known limitations:
+;;
+;; 1. you can't have more than one color on one "physical line".
+;;    This is becuse fringes operate on "per-line" basis and it is
+;;    only possible to set face for bitmap for the whole line.
+;; 2. if you are at the end of file the indicators are displaied
+;;    incorrectly. This is because it's impossible to set fringe
+;;    bitmaps for lines past the end of file.
+;;
+;; Both of these are currently imposible to fix.
+
+;;; Code:
+
+(require 'fringe)
+
+(defvar ind-managed-indicators nil
+  "Managed indicators.  Position of these on the fringe is
+automatically updated when window properties change.
+
+These are managed automatically by `indicators-mode'.  However,
+you can manage your own lists and pass them to `ind-update'
+function to be updated.")
+(make-variable-buffer-local 'ind-managed-indicators)
+
+(defun ind--pos-at-line (line)
+  "Return the starting point of LINE."
+  (save-excursion
+    (goto-line line)
+    (point)))
+
+(defun ind--line-at-pos (pos)
+  "Return the line at position POS."
+  (count-lines (point-min) pos))
+
+(defmacro ind--number-of-lines ()
+  "Return number of lines in buffer."
+  (1- (line-number-at-pos (point-max))))
+
+(defun ind-update-event-handler (&optional a b c d e f g h)
+  "Function that is called by the hooks to redraw the fringe bitmaps."
+  (ignore-errors
+    (ind-update)))
+
+(defvar ind-indicator-height 1
+  "Height of an indicator in pixels.
+The value of 1 works best, but values up to `frame-char-height'
+are possible.")
+
+(defun ind-update (&optional mlist)
+  "Update managed indicators.
+If optional argument LIST is non-nil update indicators on that list."
+  (interactive)
+  (let ((managed-list (or mlist (bound-and-true-p ind-managed-indicators))))
+    (when managed-list
+      (let* ((max-line (ind--number-of-lines))
+             (height (min max-line (window-body-height)))
+             (px-height (* height (frame-char-height)))
+             ind-list
+             buckets buckets-face)
+        ;; here we need to compute the values of functions (if there
+        ;; are some) on this managed-list and then sort it
+        (setq ind-list (sort (mapcar (lambda (pair) (cons (ind--get-indicator-pos (car pair))
+                                                          (cdr pair))) managed-list)
+                             (lambda (a b) (< (car a) (car b)))))
+        ;; next we need to compute their relative "pixel" positions
+        (setq ind-list
+              (mapcar (lambda (pair)
+                        (cons (floor (* (/ (float (ind--line-at-pos (car pair))) (float max-line))
+                                        (float px-height)))
+                              (cdr pair)))
+                      ind-list))
+        ;; parition the indicators into buckets for the same line
+        (mapc (lambda (pair)
+                (let* ((line (/ (car pair) (frame-char-height)))
+                       (ov-line (1+ (% (car pair) (frame-char-height)))))
+                  (if (plist-member buckets line)
+                      (let ((bucket (plist-get buckets line))
+                            (bucket-face (plist-get buckets-face line)))
+                        (setq buckets
+                              (plist-put buckets line
+                                         (push ov-line bucket)))
+                        (when (> (plist-get (cdr pair) :priority)
+                                 (plist-get bucket-face :priority))
+                          (setq buckets-face (plist-put buckets-face line (cdr pair)))))
+                    (setq buckets (plist-put buckets line (list ov-line)))
+                    (setq buckets-face (plist-put buckets-face line (cdr pair))))))
+              ind-list)
+        ;; and now create the overlays
+        (remove-overlays (point-min) (point-max) 'ind-indicator t)
+        (let ((top (window-start)))
+          (while buckets
+            (let* ((line (car buckets))
+                   (line-ov (cadr buckets))
+                   (line-bitmap (make-symbol (concat "ovbitmap" (int-to-string line))))
+                   (fringe-display-prop (list 'right-fringe
+                                              line-bitmap
+                                              (plist-get (cadr buckets-face) :face)))
+                   (fringe-text (propertize "!" 'display fringe-display-prop))
+                   (ov (make-overlay 1 1)))
+              (overlay-put ov 'before-string fringe-text)
+              (overlay-put ov 'priority (plist-get (cadr buckets-face) :priority))
+              (overlay-put ov 'ind-indicator t)
+              (define-fringe-bitmap line-bitmap (ind--create-bitmap line-ov))
+              (save-excursion
+                (goto-char top)
+                (forward-line line)
+                (move-overlay ov (point) (point))))
+            (setq buckets (cddr buckets))
+            (setq buckets-face (cddr buckets-face))))))))
+
+(defun ind--create-bitmap (lines)
+  "Create the bitmap according to LINES.
+
+LINES is a list of numbers where each number indicate there
+should be a line drawn on that line in the bitmap.  The numbers
+of lines need to be in reverse order.
+
+For example arugment (10 5 1) will return a bitmap [255 0 0 0 255
+0 0 0 0 255 0 0 0 0 0] for 15 pixel high line."
+  (let (lst (it (frame-char-height)))
+    (while (> it 0)
+      (if (not (member it lines))
+          (progn
+            (push 0 lst)
+            (setq it (1- it)))
+        (dotimes (i ind-indicator-height)
+          (push 255 lst))
+        (setq it (- it ind-indicator-height))))
+    (vconcat lst)))
+
+(defun ind--get-indicator-pos (pos-or-fun)
+  "Return the beginning position for line on which POS-OR-FUN is.
+POS-OR-FUN can be an integer, marker or a function.
+
+If POS-OR-FUN is a nullary function this function is used to get
+a buffer position P, then position of the beginning of line on
+which P is is returned."
+  (let ((pos (if (integer-or-marker-p pos-or-fun)
+                 pos-or-fun
+               (funcall pos-or-fun))))
+    (save-excursion
+      (goto-char pos)
+      (beginning-of-line)
+      (point))))
+
+(defun* ind-create-indicator-at-line (line
+                                   &key
+                                   (managed nil)
+                                   (face font-lock-warning-face)
+                                   (priority 10))
+  "Add an indicator on LINE.
+
+See `ind-create-indicator' for values of optional arguments."
+  (ind-create-indicator (ind--pos-at-line line)
+                     :managed managed
+                     :face face
+                     :priority priority))
+
+(defun* ind-create-indicator-at-marker (point
+                                     &key
+                                     (managed nil)
+                                     (face font-lock-warning-face)
+                                     (priority 10))
+  "Add a marker indicator on position POINT.
+This means the position updates as the text is inserted/removed.
+
+See `ind-create-indicator' for values of optional arguments."
+  (let ((m (point-marker)))
+    (set-marker m point)
+    (ind-create-indicator m
+                       :managed managed
+                       :face face
+                       :priority priority)))
+
+(defun* ind-create-indicator (pos
+                           &key
+                           (managed nil)
+                           (face font-lock-warning-face)
+                           (priority 10))
+  "Add an indicator to position POS.
+
+Keyword argument FACE is a face to use when displaying the bitmap
+for this indicator.  Default value is `font-lock-warning-face'.
+
+Keyword argument PRIORITY determines the face of the bitmap if
+more indicators are on the same physical line.  Default value is
+10."
+  (unless indicators-mode
+    (indicators-mode t))
+  (let ((indicator (cons pos (list :face face :priority priority))))
+    (when managed
+      (push indicator ind-managed-indicators)
+      (ind-update))
+    indicator))
+
+(defun ind-clear-indicators ()
+  "Remove all indicators managed by `indicators-mode'."
+  (remove-overlays (point-min) (point-max) 'ind-indicator t)
+  (setq ind-managed-indicators nil))
+
+(define-minor-mode indicators-mode
+  "Toggle indicators mode."
+  :init-value nil
+  :lighter " Ind"
+  :group 'indicators-mode
+  (if indicators-mode
+      (progn
+        (add-hook 'window-scroll-functions 'ind-update-event-handler nil t)
+        (add-hook 'window-configuration-change-hook 'ind-update-event-handler nil t)
+        (add-hook 'after-change-functions 'ind-update-event-handler nil t))
+    (remove-hook 'window-scroll-functions 'ind-update-event-handler t)
+    (remove-hook 'window-configuration-change-hook 'ind-update-event-handler t)
+    (remove-hook 'after-change-functions 'ind-update-event-handler t)
+    (ind-clear-indicators)))
+
+(provide 'indicators)
+
+;;; indicators.el ends here
